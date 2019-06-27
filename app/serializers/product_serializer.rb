68406d9717bb98a01e76089d51245c0907e904b5
Daniel Schneider <daschne8@gmail.com>
2019-06-27 15:39:55 +0000

    Done.

--- app/serializers/product_serializer.rb
+++ app/serializers/product_serializer.rb
@@ -2,10 +2,10 @@ class ProductSerializer
 
   def self.serialize(product)
     serialied_product = '{'
-    serialied_product += '"name: "' + product.name + ', '
-    serialied_product += '"price: "' + product.price + ', '
-    serialied_product += '"inventory: "' + product.inventory + ', '
-    serialied_product += '"description: "' + product.description
+    serialied_product += '"name": "' + product.name + '", '
+    serialied_product += '"price": ' + product.price.to_s + ', '
+    serialied_product += '"inventory": ' + product.inventory.to_s + ', '
+    serialied_product += '"description": "' + product.description + '"'
     serialied_product += '}'
     return serialied_product
   end
